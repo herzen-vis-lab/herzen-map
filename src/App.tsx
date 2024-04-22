fb12c90fee6b50424e803c60955e62cebd05d1db
Artem Shmarev <154342288+ashmarev-testing@users.noreply.github.com>
2024-04-22 10:36:58 +0000

    Доработка карты

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Map from "./components/Map/Map";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import Header from "./components/Header/Header";
+import { apiKey, lang } from "./constants/constants";
 
 function App() {
   return (
-    <div className="App">
-      <Map />
-    </div>
+    <>
+      <Header />
+      <YMaps query={{ apikey: apiKey, lang: lang }}>
+        <Map
+          defaultState={{
+            center: [55.75, 37.57],
+            zoom: 9,
+            controls: ["zoomControl", "fullscreenControl"],
+          }}
+          modules={["control.ZoomControl", "control.FullscreenControl"]}
+        >
+          <Placemark
+            modules={["geoObject.addon.balloon"]}
+            defaultGeometry={[55.75, 37.57]}
+            properties={{
+              balloonContentBody:
+                "This is balloon loaded by the Yandex.Maps API module system",
+            }}
+          />
+        </Map>
+      </YMaps>
+      ;
+    </>
   );
 }
 
--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,9 +1,5 @@
 import { YMapLocation } from "@yandex/ymaps3-types/imperative/YMap";
 
 export const location: YMapLocation = { center: [37.95, 55.65], zoom: 10 };
-export const apiKey = "3fac8c1a-d9ff-44e9-b96f-a5b6b8490d9a";
+export const apiKey: string = "3fac8c1a-d9ff-44e9-b96f-a5b6b8490d9a";
 export const lang = "ru_RU";
-export const initialPoint: YMapLocation = {
-  center: [37.622504, 55.753215],
-  zoom: 10,
-};
